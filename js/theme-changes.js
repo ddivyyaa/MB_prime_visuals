8ade88c6025159187ed4f5ba1b9b0c80528bd88d
thetechyuga <yugandhar5559@gmail.com>
2026-02-22 13:30:46 +0530

    Initial commit - photography website

---
+++ js/theme-changes.js
@@ -0,0 +1,25 @@
+const lightTheme = {
+  'html': 'html-light',
+  'body': 'body-light',
+  'container-header': 'container-header-light',
+  'logo': 'logo-light',
+  'nav': 'nav-light',
+  'theme-switcher': 'theme-switcher-light',
+  'hamburger-button': 'hamburger-button-light',
+  'container-hero': 'container-hero-light',
+  'button': 'button-light',
+  'button_colored': 'button_colored-light',
+  'section-title': 'section-title-light',
+  'skill-item': 'skill-item-light',
+  'skill-description': 'skill-description-light',
+  'price-list__card': 'price-list__card-light',
+  'card-price': 'card-price-light',
+  'text-gold': 'text-gold',
+  'contacts': 'contacts-light',
+  'contact-form': 'contact-form-light',
+  'social-link': 'social-link-light',
+  'credits': 'credits-light',
+  'footer': 'footer-light',
+}
+
+export default lightTheme;
